5830b565e6357f8be10b0cde62b7fe33d0738e55
Nikhil Dev <nikhil@nikhildev.com>
2016-06-28 14:05:40 +0530

    Move app folder to src to make sense of source vs dist

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -39,7 +39,7 @@ const reload = browserSync.reload;
 
 // Lint JavaScript
 gulp.task('lint', () =>
-  gulp.src('app/scripts/**/*.js')
+  gulp.src('src/scripts/**/*.js')
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failOnError()))
@@ -47,7 +47,7 @@ gulp.task('lint', () =>
 
 // Optimize images
 gulp.task('images', () =>
-  gulp.src('app/images/**/*')
+  gulp.src('src/images/**/*')
     .pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true
@@ -56,12 +56,12 @@ gulp.task('images', () =>
     .pipe($.size({title: 'images'}))
 );
 
-// Copy all files at the root level (app)
+// Copy all files at the root level (src)
 gulp.task('copy', () =>
   gulp.src([
     'CNAME',
-    'app/**/*',
-    '!app/*.html',
+    'src/**/*',
+    '!src/*.html',
     'node_modules/apache-server-configs/dist/.htaccess'
   ], {
     dot: true
@@ -85,11 +85,11 @@ gulp.task('styles', () => {
 
   // For best performance, don't add Sass partials to `gulp.src`
   return gulp.src([
-    'app/styles/**/*.scss',
-    'app/styles/**/*.css',
+    'src/styles/**/*.scss',
+    'src/styles/**/*.css',
     // Vendor CSS
-    'app/vendor/**/*.css',
-    'app/vendor/**/*.scss'
+    'src/vendor/**/*.css',
+    'src/vendor/**/*.scss'
   ])
     .pipe($.newer('.tmp/styles'))
     .pipe($.sourcemaps.init())
@@ -113,7 +113,7 @@ gulp.task('scripts', () =>
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
-      './app/scripts/main.js'
+      './src/scripts/main.js'
       // Other scripts
     ])
       .pipe($.newer('.tmp/scripts'))
@@ -131,9 +131,9 @@ gulp.task('scripts', () =>
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
-  return gulp.src('app/**/*.html')
+  return gulp.src('src/**/*.html')
     .pipe($.useref({
-      searchPath: '{.tmp,app}',
+      searchPath: '{.tmp,src}',
       noAssets: true
     }))
 
@@ -169,14 +169,14 @@ gulp.task('serve', ['scripts', 'styles'], () => {
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
     // https: true,
-    server: ['.tmp', 'app'],
+    server: ['.tmp', 'src'],
     port: 3000
   });
 
-  gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
-  gulp.watch(['app/images/**/*'], reload);
+  gulp.watch(['src/**/*.html'], reload);
+  gulp.watch(['src/styles/**/*.{scss,css}'], ['styles', reload]);
+  gulp.watch(['src/scripts/**/*.js'], ['lint', 'scripts', reload]);
+  gulp.watch(['src/images/**/*'], reload);
 });
 
 // Build and serve the output from the dist build
@@ -218,7 +218,7 @@ gulp.task('pagespeed', cb =>
 
 // Copy over the scripts that are used in importScripts as part of the generate-service-worker task.
 gulp.task('copy-sw-scripts', () => {
-  return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
+  return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'src/scripts/sw/runtime-caching.js'])
     .pipe(gulp.dest('dist/scripts/sw'));
 });
 
@@ -226,7 +226,7 @@ gulp.task('copy-sw-scripts', () => {
 // an in-depth explanation of what service workers are and why you should care.
 // Generate a service worker file that will provide offline functionality for
 // local resources. This should only be done for the 'dist' directory, to allow
-// live reload to work as expected when serving from the 'app' directory.
+// live reload to work as expected when serving from the 'src' directory.
 gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
   const rootDir = 'dist';
   const filepath = path.join(rootDir, 'service-worker.js');
