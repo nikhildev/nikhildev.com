a9b5795fa31452b03be22f4ce208aab79a8df67e
Nikhil Dev <nikhil@nikhildev.com>
2017-01-24 22:57:18 +0200

    Added Resume component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Header from './Header/Header';
 import Skills from './Skills/Skills';
 import AboutMe from './AboutMe/AboutMe';
+import Resume from './Resume/Resume';
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <div id="content" className="flex-container flex-layout-row flex-align-start-start">
           <AboutMe />
           <Skills />
+          <Resume />
         </div>
       </div>
     );
