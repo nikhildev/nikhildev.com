12f153d0fe9d47c69cfbeed5f029d75a6a4df254
Nikhil Dev <nikhil@nikhildev.com>
2017-01-24 22:40:38 +0200

    Added SocialLinks

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './Header.css';
+import SocialLinks from '../SocialLinks/SocialLinks';
 
 class Header extends Component {
   render() {
@@ -9,14 +10,16 @@ class Header extends Component {
               className="flex-container flex-layout-row flex-align-start-center">
 
         <div id="app-title"
-             className="flex-container flex-layout-column flex-align-start-start">
+             className="flex-container flex-layout-column flex-align-start-start flex">
           <span id="my-name">Nikhil Dev</span>
           <span id="designation">
             Frontend Engineer
           </span>
         </div>
 
-        <div id="social-icons"></div>
+
+
+        <SocialLinks />
 
       </header>
     );
