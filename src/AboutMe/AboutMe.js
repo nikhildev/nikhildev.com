275b3b6ba6931f033df8b74432a0313acea2dd44
Nikhil Dev <nikhil@nikhildev.com>
2017-01-24 23:21:50 +0200

    Correcting age

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -19,7 +19,7 @@ class AboutMe extends Component {
         <div className="card-content flex-container flex-layout-column">
           Hello, my name is Nikhil.
 
-          I'm a 33 year old Frontend Engineer from Hyderabad, India.
+          I'm a 34 year old Frontend Engineer from Hyderabad, India.
           I love building cool stuff in JavaScript all day long.
           I also love photography, weight training and playing the guitar.
         </div>
