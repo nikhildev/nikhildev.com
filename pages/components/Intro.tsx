f90efddcb7656fd1127f89c85445330d216df263
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2022-01-31 18:41:42 +0200

    First commit of nextjs app

---
+++ pages/components/Intro.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <p>
+      Hi, my name is Nikhil and I'm a 39 year old Engineering team lead working
+      in Finland
+    </p>
+  );
+};
+
+export default Intro;
