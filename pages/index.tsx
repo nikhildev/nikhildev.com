ee42b8be2de46987811514dacced5d4f0fba7bfb
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2022-02-09 08:29:35 +0200

    Reorganising components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import Intro from "../components/Intro";
 import styles from "../styles/Home.module.css";
-import Intro from "./components/Intro";
 
 const Home: NextPage = () => {
   return (
