7bc811ef7a9dd582f058f821c15a4a8a1e1fbaaf
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2022-04-06 09:39:30 +0300

    Adding square image for intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
-        <img src="me.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+        <img src="me_square.jpg" className="max-w-sm rounded-lg shadow-2xl" />
         <div>
           <h1 className="text-6xl text-primary font-bold">Hi!</h1>
           <p className="py-6 text-xl text-base-content">
