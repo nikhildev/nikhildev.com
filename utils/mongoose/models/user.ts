7b67366134dd5602e7c9aae30cfd311ba7f7cb05
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2022-04-21 05:41:41 +0300

    Better typing or user document

--- utils/mongoose/models/user.ts
+++ utils/mongoose/models/user.ts
@@ -1,15 +1,26 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  uid: String,
-  displayName: String,
-  email: String,
-  photoURL: String,
-  createdAt: mongoose.Schema.Types.Date,
-  modifiedAt: mongoose.Schema.Types.Date,
-});
+const userSchema = new mongoose.Schema(
+  {
+    uid: String,
+    displayName: String,
+    email: String,
+    photoURL: String,
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.models.User || mongoose.model("User", userSchema);
+export interface UserDocument extends mongoose.Document {
+  _id: mongoose.Schema.Types.ObjectId;
+  uid: String;
+  displayName: String;
+  email: String;
+  photoURL: String;
+  createdAt: Date;
+  modifiedAt: Date;
+}
+
+const User =
+  mongoose.models.User || mongoose.model<UserDocument>("User", userSchema);
 
 export default User;
