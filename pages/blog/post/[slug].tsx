dc6424d41cb33b43211f5b57d1139e3928522182
Nikhil Dev Chunchu <nikhil.dev@wolt.com>
2022-04-24 12:07:35 +0300

    Adding a button to edit a post

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -7,6 +7,7 @@ import { PostT } from "lib/types";
 import Page from "components/Page";
 import Avatar from "components/Avatar";
 import { dateStringToReadable } from "lib/common/helpers";
+import Link from "next/link";
 
 const getPostBySlug = (slug: string) =>
   fetch(`/api/blog/post/${slug}`).then((res) => res.json());
@@ -28,20 +29,25 @@ const BlogHome = () => {
           <h1 className="text-5xl text-yellow-500">
             <strong>{data.title}</strong>
           </h1>
-          <div className="my-6 text-sm inline-flex">
-            <Avatar
-              displayName={data?.author.displayName}
-              src="/me_square.jpg"
-              className="my-auto"
-            />
-            <div className="flex flex-col ml-2">
-              <span className="text-primary-content">
-                <strong>Nikhil Dev Chunchu</strong>
-              </span>
-              <span className="text-xs text-secondary-content">
-                {dateStringToReadable(data.updatedAt)}
-              </span>
+          <div className="my-6 text-sm flex flex-row">
+            <div className="flex flex-row grow">
+              <Avatar
+                displayName={data?.author.displayName}
+                src="/me_square.jpg"
+                className="my-auto"
+              />
+              <div className="flex flex-col ml-2">
+                <span className="text-primary-content">
+                  <strong>Nikhil Dev Chunchu</strong>
+                </span>
+                <span className="text-xs text-secondary-content">
+                  {dateStringToReadable(data.updatedAt)}
+                </span>
+              </div>
             </div>
+            <span className="btn btn-outline btn-small btn-secondary w-auto">
+              <Link href={`/blog/editor/${data.slug}`}>Edit post</Link>
+            </span>
           </div>
           <RichText content={data.body} />
         </div>
